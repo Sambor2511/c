a7b7be5fd9e4af3e0ee255b6bf58a5b8a999915b
Sok Sambor <samborsok12345@gmail.com>
2024-07-01 19:56:38 +0700

    hello world

---
+++ code/helloworld.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main(void)
+{
+    // Hello World
+//    printf("Hello, World!\n");  // Actually do the work here
+
+    // Increment
+//    int i,j;
+//    i = 10;
+//    j = 5 + i++;  // Compute 5 + i, _then_ increment i
+//
+//    printf("%d, %d\n", i, j);  // Prints 11, 15
+//
+//    i = 10;
+//    j = 5 + ++i;  // Increment i, _then_ compute 5 + i
+//
+//    printf("%d, %d\n", i, j);  // Prints 11, 16
+
+    // Ternary
+//    int y = 0;
+//    int x = 16;
+//    // Actually do the work here
+//    y += x > 10? 17: 37;
+//    printf("Hello %d", y);
+//// is equivalent to this non-expression:
+//
+////    if (x > 10)
+////        y += 17;
+////    else
+////        y += 37;
+
+    // Comma Operator
+//    int x = (1, 2, 3);
+//    printf("x is %d\n", x);  // Prints 3, because 3 is rightmost in the comma list
+//
+//// But even that’s pretty contrived. One common place the comma operator is used is in for loops to do multiple things in each section of the statement:
+//    int i,j;
+//    for (i = 0, j = 10; i < 100; i++, j++)
+//        printf("%d, %d\n", i, j);
+
+
+}
