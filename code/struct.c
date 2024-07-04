3d82a40b447e9a42474a58677d2ce4835e9b2dfd
Sok Sambor <samborsok12345@gmail.com>
2024-07-04 22:02:46 +0700

    updated struct.c

--- code/struct.c
+++ code/struct.c
@@ -24,3 +24,31 @@ struct car saturn = {"Saturn SL/2", 16000.99, 175};
 printf("Name: %s\n", saturn.name);
 printf("Price: %f\n", saturn.price);
 printf("Top Speed: %d km\n", saturn.speed);
+
+struct car saturn = {.speed=175, .name="Saturn SL/2"};
+
+// Passing Structs to Functions
+#include <stdio.h>
+
+struct car {
+    char *name;
+    float price;
+    int speed;
+};
+
+int main(void)
+{
+    struct car saturn = {.speed=175, .name="Saturn SL/2"};
+
+    // Pass a pointer to this struct car, along with a new,
+    // more realistic, price:
+    set_price(&saturn, 799.99);
+
+    printf("Price: %f\n", saturn.price);
+}
+void set_price(struct car *c, float new_price) {
+    c.price = new_price;  // ERROR!!
+}
+void set_price(struct car *c, float new_price) {
+    (*c).price = new_price;  // Works, but is ugly and non-idiomatic :(
+}
